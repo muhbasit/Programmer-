4d76f25516651964efd898255b999c4b363568bc
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2024-09-23 20:54:04 +0400

    Create syntaxWithComments.cs

---
+++ C# /Unit 1/Tutorial /syntaxWithComments.cs
@@ -0,0 +1,12 @@
+using System; // Importing the System namespace to access built-in C# functionalities like input/output.
+
+namespace BilalStudio // Defining a namespace called BilalStudio to encapsulate the program.
+{
+    class Program // Defining a class named Program, which serves as the container for the Main method.
+    {
+        static void Main(string[] args) // The Main method, the entry point of the program where execution begins.
+        {
+            Console.WriteLine("Welcome to Bilal Studio (BS)!"); // Using the Console.WriteLine method to print the welcome message to the console.
+        }
+    }
+}
