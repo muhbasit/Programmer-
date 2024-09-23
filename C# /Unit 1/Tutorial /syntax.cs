c70cc08dfbd337eaf713d7ba05c004741634556d
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2024-09-23 20:45:22 +0400

    Create syntax.cs

---
+++ C# /Unit 1/Tutorial /syntax.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BilalStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bilal Studio (BS)!");
+        }
+    }
+}
