d7139427511a1cc53444714aa7e69023c08928c9
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2024-09-26 03:50:44 +0400

    Create Loops.cs

---
+++ C# /Unit 2 /Loops.cs
@@ -0,0 +1,164 @@
+switch (variable)
+{
+    case value1:
+        // Code to execute if variable equals value1
+        break;
+    case value2:
+        // Code to execute if variable equals value2
+        break;
+    default:
+        // Code to execute if no case matches
+        break;
+}
+
+int day = 2;
+
+int day = 2;
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    default:
+        Console.WriteLine("Invalid day");
+        break;
+}
+
+int a = 5;
+int b = 10;
+int sum = a + b;
+Console.WriteLine("The sum is: " + sum);
+
+int number = 20;
+
+if (number > 10)
+{
+    Console.WriteLine("The number is greater than 10.");
+}
+
+int day = 3;
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    default:
+        Console.WriteLine("Invalid day");
+        break;
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Iteration: " + i);
+}
+
+int count = 0;
+
+while (count < 5)
+{
+    Console.WriteLine("Count: " + count);
+    count++;
+}
+
+int num = 0;
+
+do
+{
+    Console.WriteLine("Number: " + num);
+    num++;
+} while (num < 5);
+
+if (condition1)
+{
+    // Code to execute if condition1 is true
+}
+else if (condition2)
+{
+    // Code to execute if condition2 is true
+}
+else
+{
+    // Code to execute if both conditions are false
+}
+
+int number = 5;
+
+if (number > 5)
+{
+    Console.WriteLine("The number is greater than 5.");
+}
+else if (number == 5)
+{
+    Console.WriteLine("The number is equal to 5.");
+}
+else
+{
+    Console.WriteLine("The number is less than 5.");
+}
+
+switch (variable)
+{
+    case value1:
+        // Code to execute if variable equals value1
+        break;
+    case value2:
+        // Code to execute if variable equals value2
+        break;
+    default:
+        // Code to execute if no case matches
+        break;
+}
+
+int day = 2;
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    default:
+        Console.WriteLine("Invalid day");
+        break;
+}
+
+while (condition)
+{
+    // Code to be executed repeatedly
+}
+
+do
+{
+    // Code to be executed repeatedly
+} while (condition);
+
+foreach (var item in collection)
+{
+    // Code to be executed for each item
+}
+
+string[] fruits = { "Apple", "Banana", "Cherry" };
+
+foreach (var fruit in fruits)
+{
+    Console.WriteLine("Fruit: " + fruit);
+}
